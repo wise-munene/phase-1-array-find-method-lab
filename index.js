64e44dc62ba7e8255bf3be645b62b49409341eba
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-09-08 13:04:55 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record =[
+    {year : "2015", result: "W"},
+    {year : "2014", result: "N/A"},
+    {year :"2013", result: "L"},
+]
+// function that singles out a single result 
+function superbowlWin(records) {
+  const win = records.find(function(game) {
+    return game.result === "W";
+  });
+  return win ? win.year : undefined;
+}
